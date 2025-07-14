e85364e3b42e406177d6134538ae966096844be1
bmo <bradley.s.morris@gmail.com>
2025-07-14 14:00:23 -0600

    Remember last cursor position in file.

--- lua/options.lua
+++ lua/options.lua
@@ -15,3 +15,15 @@ vim.cmd('autocmd! nvim.popupmenu')
 
 -- Right click copies selected text
 vim.keymap.set('v', '<RightMouse>', '"+y')
+
+-- Restore cursor position when opening files (row AND column)
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line("'\"")
+    local col = vim.fn.col("'\"")
+    if line > 1 and line <= vim.fn.line("$") then
+      vim.api.nvim_win_set_cursor(0, {line, col - 1})  -- col is 0-indexed in nvim_win_set_cursor
+    end
+  end,
+})
